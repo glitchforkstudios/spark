29ddb6e0e3565f59f7c92cd2ad57ab165025df99
OverLack <overlack.yt@gmail.com>
2025-12-16 23:16:16 +0000

    v1.2.0: added info.py - made bot responses ephemeral

--- cogs/info.py
+++ cogs/info.py
@@ -3,24 +3,31 @@ from discord import app_commands
 from discord.ext import commands
 import datetime
 
-class Ping(commands.Cog):
+class Info(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(
-        name="ping",
-        description="Check if the bot is alive"
+        name="info",
+        description="Shows some info about this bot"
     )
-    async def ping(self, interaction: discord.Interaction):
+    async def info(self, interaction: discord.Interaction):
         embed = discord.Embed(
-            title="Pong!",
-            description="Bot is online and responding",
+            title="Bot info",
+            description="Here is some info about Spark (me!)",
             colour=discord.Colour.from_str("#f1b50f")
         )
 
         embed.add_field(
-            name="Latency",
-            value=f"{round(self.bot.latency * 1000)} ms"
+            name="About me",
+            value=f"Spark is a utility bot for the GlitchFork Studios server. My goal is to keep the server managed, safe and enjoyable for all of our members.",
+            inline=False
+        )
+
+        embed.add_field(
+            name="My Creator",
+            value=f"Spark was created by **OverLack**, the founder of GlitchFork Studios. He also codes, maintains and hosts me all from within his own home.",
+            inline=False
         )
 
         embed.set_footer(
@@ -34,4 +41,4 @@ class Ping(commands.Cog):
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(Ping(bot))
+    await bot.add_cog(Info(bot))
